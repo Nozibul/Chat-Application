431811a6e462b37109ee5790b8b6c28dca420a48
Nozibul <nozibulislamspi@gmail,com>
2023-03-18 11:38:41 +0600

    add loagin feature

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,41 @@
-import { Link } from "react-router-dom";
-import logoImage from "../assets/images/logo-light.svg";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Error from "../components/ui/Error";
+import { useLoginMutation } from "../features/auth/authApi";
 
 export default function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+     
+    const [login, {data, isLoading, error: responseError}] = useLoginMutation()
+   
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        if(responseError?.data) setError(responseError.data);
+        if(data?.accessToken && data?.user) navigate("/inbox");
+    }, [data, responseError, navigate]);
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+         setError('')
+
+        login({email, password})   
+    }
+
     return (
         <div className="grid place-items-center h-screen bg-[#F9FAFB">
             <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
                 <div className="max-w-md w-full space-y-8">
                     <div>
-                        <Link to="/">
-                            <img
-                                className="mx-auto h-12 w-auto"
-                                src={logoImage}
-                                alt="Learn with sumit"
-                            />
-                        </Link>
+                    
                         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                             Sign in to your account
                         </h2>
                     </div>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
-                        <input type="hidden" name="remember" value="true" />
+                    <form className="mt-8 space-y-6" onSubmit={handleSubmit} method="POST">
                         <div className="rounded-md shadow-sm -space-y-px">
                             <div>
                                 <label
@@ -37,6 +52,8 @@ export default function Login() {
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Email address"
+                                    value={email}
+                                    onChange={(e)=> setEmail(e.target.value)}
                                 />
                             </div>
                             <div>
@@ -51,6 +68,8 @@ export default function Login() {
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Password"
+                                    value={password}
+                                    onChange={(e)=>setPassword(e.target.value)}
                                 />
                             </div>
                         </div>
@@ -68,14 +87,14 @@ export default function Login() {
 
                         <div>
                             <button
+                               disabled={isLoading}
                                 type="submit"
                                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
                             >
                                 Sign in
                             </button>
                         </div>
-
-                        <Error message="There was an error" />
+                        {error !== "" && <Error message={error} />}
                     </form>
                 </div>
             </div>
