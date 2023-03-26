af5e0c601bc6e559c6adf74e2de1460ce4cfbff4
Nozibul <nozibulislamspi@gmail,com>
2023-03-26 12:36:45 +0600

    chat head feature

--- src/components/inbox/chatbody/ChatBody.js
+++ src/components/inbox/chatbody/ChatBody.js
@@ -30,8 +30,7 @@ const ChatBody=()=>{
       }else if (!isLoading && !isError && messages?.length > 0){
           content = <>
                  <ChatHead
-                    avatar="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
-                    name="Akash Ahmed"
+                    messages={messages[0]}
                 />
                 <Messages messages={messages}/>
                 <Options />
